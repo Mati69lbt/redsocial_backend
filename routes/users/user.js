b74738c03fb8453c2c4e7168106f1190ebe74550
Mati69lbt <mati69_lbt@hotmail.com>
2024-01-20 11:42:36 -0300

    subir imagen

--- routes/users/user.js
+++ routes/users/user.js
@@ -1,4 +1,16 @@
 const { Router } = require("express");
+("uploadsavatars");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/avatars");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "Avatar-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploads = multer({ storage });
 
 const auth = require("../../middlewares/auth");
 
@@ -8,6 +20,7 @@ const login = require("../../controllers/users/login_user");
 const profile = require("../../controllers/users/datos_del_usuario");
 const listado = require("../../controllers/users/listado_de_usuarios");
 const update = require("../../controllers/users/actualizar_perfil");
+const foto_de_perfil = require("../../controllers/users/foto_de_perfil");
 
 const router = Router();
 
@@ -20,6 +33,7 @@ router.post("/login", login);
 router.get("/profile/:id", auth, profile);
 router.get("/list/:page?", auth, listado);
 router.put("/update", auth, update);
+router.get("/upload", [auth, uploads.single("file0")], foto_de_perfil);
 
 //exportar
 module.exports = router;
